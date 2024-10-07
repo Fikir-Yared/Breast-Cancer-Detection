303ca6da40191a3a9705163d718fd157d0cbd3a7
Fikir-Yared <141673497+Fikir-Yared@users.noreply.github.com>
2024-10-07 21:15:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request,redirect,url_for
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user
 import os
@@ -18,44 +18,37 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 
-
-
-
-
-
-
-
 class Users(UserMixin, db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(250), unique=True, nullable=False)
-	password = db.Column(db.String(250), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
 
 
 db.init_app(app)
 
 
 with app.app_context():
-	db.create_all()
+    db.create_all()
 
 
 @login_manager.user_loader
 def loader_user(user_id):
-	return Users.query.get(user_id)
+    return Users.query.get(user_id)
 
 
 @app.route('/register', methods=["GET", "POST"])
 def register(error=''):
-      
+
     if request.method == "POST":
         try:
             user = Users(username=request.form.get("username"),
-                        password=request.form.get("password"))
+                         password=request.form.get("password"))
             db.session.add(user)
             db.session.commit()
             return redirect(url_for("login"))
         except:
-             return render_template("sign_up.html",error="Username already taken!!")
-    return render_template("sign_up.html",error=error)
+            return render_template("sign_up.html", error="Username already taken!!")
+    return render_template("sign_up.html", error=error)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -68,38 +61,14 @@ def login(error=''):
                 login_user(user)
                 return redirect(url_for("home"))
         except:
-            return render_template("login.html",error="Wrong username or password!")
-    return render_template("login.html",error=error)
+            return render_template("login.html", error="Wrong username or password!")
+    return render_template("login.html", error=error)
 
 
 @app.route("/logout")
 def logout():
-	logout_user()
-	return redirect(url_for("home"))
-
-
-
-
-     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    logout_user()
+    return redirect(url_for("home"))
 
 
 @app.route("/")
@@ -109,64 +78,62 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/service",methods=["POST","GET"])
+@app.route("/service", methods=["POST", "GET"])
 def service():
-    result=['','']
+    result = ['', '']
     if request.method == 'POST':
         try:
-            
+
             if request.form['submit_button'] == 'reset':
-                return render_template("service.html",result=result)
-            
-               
+                return render_template("service.html", result=result)
+
         except:
-                
+
             try:
                 if request.form['submit_button'] == 'detect':
                     print("845612345645")
             except:
-                 pass 
-            f = request.files['file'] 
-            path=os.path.join(app.config['UPLOAD_FOLDER'],f.filename)
-            f.save(os.path.join(app.config['UPLOAD_FOLDER'],f.filename))
-            value=predict_image_class(path)
-            result = "Benign" if value==[0] else "Malignant"
-            result[result,'91%']
-            return render_template("service.html", name = f.filename,result=result)  
-          
+                pass
+            f = request.files['file']
+            path = os.path.join(app.config['UPLOAD_FOLDER'], f.filename)
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
+            value = predict_image_class(path)
+            result = "Benign" if value == [0] else "Malignant"
+            result = [result, '91%']
+            return render_template("service.html", name=f.filename, result=result)
+
     else:
-        return render_template("service.html",result=result)
+        return render_template("service.html", result=result)
 
 
 @app.route("/about")
 def about():
 
     return render_template("About.html")
+
+
 @app.route("/contact")
 def contact():
 
     return render_template("contact.html")
 
- 
 
 model = tf.keras.models.load_model('first_model (3).keras')
 
+
 def load_and_preprocess_image(img_path):
-    img = image.load_img(img_path, target_size=(224, 224)) 
+    img = image.load_img(img_path, target_size=(224, 224))
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
-    img_array /= 255.0  
+    img_array /= 255.0
     return img_array
+
 
 def predict_image_class(img_path):
     img_array = load_and_preprocess_image(img_path)
     predictions = model.predict(img_array)
     predicted_class = np.argmax(predictions, axis=1)
     return predicted_class
-
-
-
-
 
 
 if "__main__" == __name__:
